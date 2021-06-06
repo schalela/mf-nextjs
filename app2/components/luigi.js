5b1745da9050b17c5241f2b71ea4596d97a26186
Sam Chalela <chalela@hey.com>
2021-06-06 21:07:07 +1000

    chore(app2): Updated app2 with image

--- app2/components/luigi.js
+++ app2/components/luigi.js
@@ -4,9 +4,11 @@ import styles from '../styles/Luigi.module.css'
 const Luigi = () => {
   return (
     <main className={styles.main}>
+      <img src="https://upload.wikimedia.org/wikipedia/en/7/73/Luigi_NSMBUDX.png" alt="Luigi" />
       <h1 className={styles.title}>
-        G'day! I'm Luigi, a microfrontend. I was born @ {(new Date()).toISOString()}
+        G'day! I'm Luigi, a microfrontend.
       </h1>
+      <span>Last time I was updated: Jun 6 2021, 9:05PM</span>
     </main>
   )
 }
