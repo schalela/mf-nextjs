e018d1c8ef2a401f9c77c1dc36b1f6ae067eafe7
Sam Chalela <chalela@hey.com>
2021-06-06 20:54:22 +1000

    chore(app1): Updated app1 with image

--- app1/components/mario.js
+++ app1/components/mario.js
@@ -4,9 +4,11 @@ import styles from '../styles/Mario.module.css'
 const Mario = () => {
   return (
     <main className={styles.main}>
+      <img src="https://upload.wikimedia.org/wikipedia/en/a/a9/MarioNSMBUDeluxe.png" alt="Mario" />
       <h1 className={styles.title}>
-        G'day! I'm Mario, a microfrontend. I was born @ {(new Date()).toISOString()}
+        G'day! I'm Mario, a microfrontend.
       </h1>
+      <span>Last time I was updated: Jun 6 2021, 8:50PM</span>
     </main>
   )
 }
