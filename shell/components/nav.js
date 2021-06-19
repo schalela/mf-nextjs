6cbed1dd3336f494e0f2bb745131312f9e1141e8
Sam Chalela <chalela@hey.com>
2021-06-20 01:16:41 +1000

    chore: renamed routes

--- shell/components/nav.js
+++ shell/components/nav.js
@@ -3,9 +3,9 @@ import styles from '../styles/Nav.module.css'
 
 const Nav = () => (
   <div className={styles.nav}>
-    <Link href="/app1">Load Mario</Link>
+    <Link href="/mario">Load Mario</Link>
     <Link href="/">Home</Link>
-    <Link href="/app2">Load Luigi</Link>
+    <Link href="/luigi">Load Luigi</Link>
   </div>
 )
 
